9cc00a9837aa62f9d8e1a0b58adbc17cec56322c
Simon Dirmeier <simd23@protonmail.com>
2022-05-01 21:15:08 +0200

    Add clojure lein profile

---
+++ profiles.clj
@@ -0,0 +1,3 @@
+{:user {:dependencies [[clj-kondo "RELEASE"] [lein-cljfmt "0.8.0"]]
+        :aliases {"clj-kondo" ["run" "-m" "clj-kondo.main"]
+                  "clj-format" ["run" "-m" "cljfmt.main"]}}}
